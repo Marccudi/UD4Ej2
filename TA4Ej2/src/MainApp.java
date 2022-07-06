c46987ffb08e758a4ea4430d929a75ce3a4b434b
Marccudi <marccudi@hotmail.es>
2022-07-06 22:08:01 +0200

    Ejercicio 2 acabado

---
+++ TA4Ej2/src/MainApp.java
@@ -0,0 +1,19 @@
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 14;
+		double a = 25.52;
+		char c = 'a';
+		int value = c;
+		
+		System.out.println("N = " + n + " A = " + a + " C = " + c);
+
+		System.out.println("N + A= " + (n+a));
+		System.out.println("A - N= " + (a-n));
+		System.out.println("C = " + c);
+		System.out.println("Valor numerico de C = " + value);
+	}
+
+}
